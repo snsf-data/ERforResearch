d3751b1e2eb8d2b1f26a4f693fa35d15f879c814
Rachel Heyard <rachel.heyard@uzh.ch>
2023-01-19 12:29:39 +0100

    more documentation changes

--- R/other_helpers.R
+++ R/other_helpers.R
@@ -4,7 +4,7 @@
 #' otherwise the user has to provide the initial values themselves. It is
 #' however recommended to use four chain. The helper function will also use the
 #' four different random generators provided by rjags.
-#' @param merging_sections should the sections be merged?, default is FALSE,
+#' @param merging_panels should the sections be merged?, default is FALSE,
 #' e.g. a ranking is done for each section/panel separately.
 #' @param ordinal_scale are the evaluation scores on an ordinal scale?,
 #' default is FALSE.
@@ -12,11 +12,11 @@
 #' the scale?, default is NULL
 #' @param heterogeneous_residuals dummy variable informing us on whether or not
 #' the residuals should be heterogeneous, default = FALSE.
-#' @param n_applications number of applications / proposals considered.
-#' @param n_voters number of voters / panel members.
-#' @param n_sections number of sections / panels, if merged, default is NULL.
+#' @param n_proposals number of applications / proposals considered.
+#' @param n_assessors number of voters / panel members.
+#' @param n_panels number of sections / panels, if merged, default is NULL.
 #' @param grades the grades given to the proposals (the whole
-#' n_applications*n_voters long vector)
+#' n_proposals*n_assessors long vector)
 #' @param seed seed used for the sampling
 #' @details It is optimal to use four chains. This also allows the sampling to
 #' use the four different samplers implemented in rjags (e.g. Wichmann-Hill,
@@ -24,13 +24,13 @@
 #'
 #' @return a list of overdispersed initial values for the two chains
 #' @export
-get_inits_overdispersed_four_chains <- function(merging_sections = FALSE,
+get_inits_overdispersed_four_chains <- function(merging_panels = FALSE,
                                                 ordinal_scale = FALSE,
                                                 point_scale = NULL,
                                                 heterogeneous_residuals = FALSE,
-                                                n_applications,
-                                                n_voters,
-                                                n_sections = NULL,
+                                                n_proposals,
+                                                n_assessors,
+                                                n_panels = NULL,
                                                 grades = NULL,
                                                 seed){
 
@@ -39,75 +39,75 @@ get_inits_overdispersed_four_chains <- function(merging_sections = FALSE,
   # Initialise them for the case where the results of the panels are NOT merged
   # and the scores are on a numeric scale:
   inits_chain1 <- # Inits for first chain
-    list(application_intercept =
-           rep.int(-4, times = n_applications),
-         voter_intercept = matrix(rep(-4, times = n_voters *
-                                        n_applications),
-                                  ncol = n_voters,
-                                  nrow = n_applications),
-         nu = rep(-2, n_voters),
+    list(proposal_intercept =
+           rep.int(-4, times = n_proposals),
+         assessor_intercept = matrix(rep(-4, times = n_assessors *
+                                        n_proposals),
+                                  ncol = n_assessors,
+                                  nrow = n_proposals),
+         nu = rep(-2, n_assessors),
          sigma = 10^{-4},
-         tau_application =  10^{-4},
-         tau_voter = 10^{-6},
+         tau_proposal =  10^{-4},
+         tau_assessor = 10^{-6},
          # need this part to get reproducible results
          .RNG.name = "base::Wichmann-Hill",
          .RNG.seed = seeds[1])
   inits_chain2 <- # Inits for second chain
-    list(application_intercept =
-           rep.int(4, times = n_applications),
-         voter_intercept = matrix(rep(4, times = n_voters *
-                                        n_applications),
-                                  ncol = n_voters,
-                                  nrow = n_applications),
-         nu = rep(2, n_voters),
+    list(proposal_intercept =
+           rep.int(4, times = n_proposals),
+         assessor_intercept = matrix(rep(4, times = n_assessors *
+                                        n_proposals),
+                                  ncol = n_assessors,
+                                  nrow = n_proposals),
+         nu = rep(2, n_assessors),
          sigma = 1,
-         tau_application = 1.5,
-         tau_voter = 1,
+         tau_proposal = 1.5,
+         tau_assessor = 1,
          # need this part to get reproducible results
          .RNG.name = "base::Marsaglia-Multicarry",
          .RNG.seed = seeds[2])
   inits_chain3 <- # Inits for third chain
-    list(application_intercept =
-           rep.int(-2, times = n_applications),
-         voter_intercept = matrix(rep(-2, times = n_voters *
-                                        n_applications),
-                                  ncol = n_voters,
-                                  nrow = n_applications),
-         nu = rep(-1, n_voters),
+    list(proposal_intercept =
+           rep.int(-2, times = n_proposals),
+         assessor_intercept = matrix(rep(-2, times = n_assessors *
+                                        n_proposals),
+                                  ncol = n_assessors,
+                                  nrow = n_proposals),
+         nu = rep(-1, n_assessors),
          sigma = 10^{-2},
-         tau_application =  10^{-2},
-         tau_voter = 10^{-3},
+         tau_proposal =  10^{-2},
+         tau_assessor = 10^{-3},
          # need this part to get reproducible results
          .RNG.name = "base::Super-Duper",
          .RNG.seed = seeds[3])
   inits_chain4 <- # Inits for fourth chain
-    list(application_intercept =
-           rep.int(2, times = n_applications),
-         voter_intercept = matrix(rep(2, times = n_voters *
-                                        n_applications),
-                                  ncol = n_voters,
-                                  nrow = n_applications),
-         nu = rep(1, n_voters),
+    list(proposal_intercept =
+           rep.int(2, times = n_proposals),
+         assessor_intercept = matrix(rep(2, times = n_assessors *
+                                        n_proposals),
+                                  ncol = n_assessors,
+                                  nrow = n_proposals),
+         nu = rep(1, n_assessors),
          sigma = 2,
-         tau_application = 2,
-         tau_voter = 2,
+         tau_proposal = 2,
+         tau_assessor = 2,
          # need this part to get reproducible results
          .RNG.name = "base::Mersenne-Twister",
          .RNG.seed = seeds[4])
 
-  if (merging_sections) {
+  if (merging_panels) {
    inits_chain1 <- # Inits for first chain
-     c(inits_chain1, list(tau_section = 10^{-6},
-                          section_intercept = rep.int(-5, times = n_sections)))
+     c(inits_chain1, list(tau_panel = 10^{-6},
+                          panel_intercept = rep.int(-5, times = n_panels)))
     inits_chain2 <- # Inits for second chain
-      c(inits_chain2, list(tau_section = 1,
-                           section_intercept = rep.int(5, times = n_sections)))
+      c(inits_chain2, list(tau_panel = 1,
+                           panel_intercept = rep.int(5, times = n_panels)))
     inits_chain3 <- # Inits for third chain
-      c(inits_chain3, list(tau_section = 10^{-3},
-                           section_intercept =rep.int(-3, times = n_sections)))
+      c(inits_chain3, list(tau_panel = 10^{-3},
+                           panel_intercept =rep.int(-3, times = n_panels)))
     inits_chain4 <- # Inits for fourth chain
-      c(inits_chain4, list(tau_section = 2,
-                           section_intercept = rep.int(3, times = n_sections)))
+      c(inits_chain4, list(tau_panel = 2,
+                           panel_intercept = rep.int(3, times = n_panels)))
   }
 
   if (ordinal_scale) {
